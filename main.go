94395fc18ba74ab2cce4e46cb7a314d857b27975
iszaqt <132551846+izsaqt@users.noreply.github.com>
2024-05-18 19:07:43 -0700

    Update main.go

--- main.go
+++ main.go
@@ -31,12 +31,12 @@ func Login(user string, password string) {
 		fmt.Println("Error sending HTTP request:", err)
 		return
 	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		fmt.Println("Failed to authenticate. Status Code:", resp.StatusCode)
 		return
 	}
-
-	defer resp.Body.Close()
 
 	for _, cookie := range resp.Cookies() {
 		if cookie.Name == "auth" {
@@ -54,6 +54,7 @@ func Login(user string, password string) {
 	fmt.Println("Status:", resp.Status)
 	fmt.Println("Response body:", string(respBody))
 }
+
 func VerifyEmailOTP(code string) {
 	apiUrl := "https://api.vrchat.cloud/api/1/auth/twofactorauth/emailotp/verify"
 
@@ -86,10 +87,42 @@ func VerifyEmailOTP(code string) {
 		fmt.Println("Error sending HTTP request:", err)
 		return
 	}
-
 	defer resp.Body.Close()
 
 	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	formattedData := formatJSON(respBody)
+	fmt.Println("Status:", resp.Status)
+	fmt.Println("Response body:", formattedData)
+}
+
+//Select Avatar
+func ChangeAvatar(authToken string, avatarID string){
+	apiSelectUrl := "https://api.vrchat.cloud/api/1/avatars/" + avatarID + "/select"
+
+	req, err := http.NewRequest("PUT", apiSelectUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating HTTP request:", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "auth=" + authToken)
+	req.Header.Add("User-Agent", "golang Client")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending HTTP request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
 		return
@@ -111,19 +144,46 @@ func formatJSON(data []byte) string {
 }
 
 func main() {
+	
+	for {
 	var Username string
 	var Password string
 
+	
 	fmt.Println("Enter your username: ")
 	fmt.Scan(&Username)
 	fmt.Println("Enter your password: ")
 	fmt.Scan(&Password)
 	fmt.Println("Making POST request...")
-
+	
 	Login(Username, Password)
+	if authCookie != "" {
+		break
+	}
+	
+
+	
+
+		fmt.Println("Login failed. Would you like to retry? (y/n)")
+		var choice string
+		fmt.Scan(&choice)
+		if choice != "y" {
+			fmt.Println("Exiting program.")
+			return
+		}
+	}
+
 
 	var code string
+
 	fmt.Println("Enter the 2FA email OTP code: ")
 	fmt.Scan(&code)
 	VerifyEmailOTP(code)
+
+	//Select Avatar
+	var avatarID string
+	
+	fmt.Println("Enter the Avatar ID you want to select: ")
+	fmt.Scan(&avatarID)
+	ChangeAvatar(authCookie, avatarID)
 }
