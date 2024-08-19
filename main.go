0e2f7cb6db55d2c891acdbf743198d0f7fc59a70
iszaqt <132551846+izsaqt@users.noreply.github.com>
2024-08-19 16:57:43 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,146 +1,56 @@
 package main
-
+		
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
 )
 
 var authCookie string
+var avatarID string
 
-func Login(user string, password string) {
-	loginURL := "https://api.vrchat.cloud/api/1/auth/user"
+var (
+	Username string
+	Password string
+	code string
+)
 
-	req, err := http.NewRequest("GET", loginURL, nil)
-	if err != nil {
-		fmt.Println("Error creating HTTP request:", err)
-		return
-	}
-
-	req.SetBasicAuth(user, password)
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Add("User-Agent", "golang Client")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Failed to authenticate. Status Code:", resp.StatusCode)
-		return
-	}
-
-	for _, cookie := range resp.Cookies() {
-		if cookie.Name == "auth" {
-			authCookie = cookie.Value
-			break
-		}
-	}
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-
-	fmt.Println("Status:", resp.Status)
-	fmt.Println("Response body:", string(respBody))
-}
-
-func VerifyEmailOTP(code string) {
-	apiUrl := "https://api.vrchat.cloud/api/1/auth/twofactorauth/emailotp/verify"
-
-	requestBody, err := json.Marshal(map[string]string{
-		"code": code,
-	})
-	if err != nil {
-		fmt.Println("Error encoding request body:", err)
-		return
-	}
-
-	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(requestBody))
-	if err != nil {
-		fmt.Println("Error creating HTTP request:", err)
-		return
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Add("User-Agent", "golang Client")
-	if authCookie != "" {
-		req.Header.Set("Cookie", "auth="+authCookie)
-	} else {
-		fmt.Println("Missing auth cookie. Please login first.")
-		return
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-
-	formattedData := formatJSON(respBody)
-	fmt.Println("Status:", resp.Status)
-	fmt.Println("Response body:", formattedData)
-}
+var choice string
 
 func main() {
-	
 	for {
-	var Username string
-	var Password string
-
-	
-	fmt.Println("Enter your username: ")
-	fmt.Scan(&Username)
-	fmt.Println("Enter your password: ")
-	fmt.Scan(&Password)
-	fmt.Println("Making POST request...")
-	
-	Login(Username, Password)
-	if authCookie != "" {
-		break
-	}
-	
-
-	
-
+		fmt.Println("Enter your username: ")
+		fmt.Scan(&Username)
+		fmt.Println("Enter your password: ")
+		fmt.Scan(&Password)
+		fmt.Println("Making POST request...")
+		
+		Login(Username, Password)
+		if authCookie != "" {
+			break
+		}
+		
 		fmt.Println("Login failed. Would you like to retry? (y/n)")
-		var choice string
 		fmt.Scan(&choice)
 		if choice != "y" {
 			fmt.Println("Exiting program.")
 			return
 		}
 	}
-
-
-	var code string
-
+	
 	fmt.Println("Enter the 2FA email OTP code: ")
 	fmt.Scan(&code)
-	VerifyEmailOTP(code)
-
-	//Select Avatar
-	var avatarID string
-	
-	fmt.Println("Enter the Avatar ID you want to select: ")
-	fmt.Scan(&avatarID)
-	ChangeAvatar(authCookie, avatarID)
+		VerifyEmailOTP(code)
+		
+		for {
+			fmt.Println("Enter the Avatar ID you want to select: ")
+			fmt.Scan(&avatarID)
+			
+			ChangeAvatar(authCookie, avatarID)
+		
+			fmt.Println("Select a different Avatar? Enter 'y' to continue, 'n' to exit program: ")
+			fmt.Scan(&choice)
+			if choice != "y" {
+			fmt.Println("Exiting program.")
+			return
+		}
+	}
 }
