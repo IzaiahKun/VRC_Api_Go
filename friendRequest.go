280638b3266741f652917972841dc8f177e65d1c
iszaqt <132551846+izsaqt@users.noreply.github.com>
2024-08-19 21:41:39 -0700

    Update friendRequest.go

--- friendRequest.go
+++ friendRequest.go
@@ -45,3 +45,43 @@ func sendFriendRq(authToken string, userID string){
 	fmt.Println("Response body:", formattedData)
 	
 }
+
+func cancelFriendrq(authToken string, userID string){
+	
+	cancelFriendrqApiURl := "https://vrchat.com/api/1/user/" + userID + "/friendRequest"
+	
+	
+	req, err := http.NewRequest("DELETE",cancelFriendrqApiURl, nil )
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", "auth=" + authToken)
+	req.Header.Add("User-Agent", "golang Client")
+	
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+	fmt.Println("Error sending HTTP request:", err)
+	return
+	}
+	defer resp.Body.Close()
+	
+	if resp.StatusCode != http.StatusOK {
+	fmt.Println("Failed to cancel friend request. Status Code:", resp.StatusCode)
+	return
+	}
+	
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+	fmt.Println("Error reading response body:", err)
+	return
+	}
+	
+	formattedData := formatJSON(respBody)
+	fmt.Println("Status:", resp.Status)
+	fmt.Println("Response body:", formattedData)
+	
+}
