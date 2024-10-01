2f88aae02a3af65763ec0fe40562fbc9046bd307
iszaqt <132551846+izsaqt@users.noreply.github.com>
2024-10-01 16:23:50 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,34 +1,45 @@
 package main
-		
+
 import (
 	"fmt"
 )
 
 var authCookie string
+var authToken string
 var avatarID string
-var userID string
 
 var (
 	Username string
 	Password string
-	code string
+	code     string
 )
 
 var choice string
 
 func main() {
+	authToken, err := loadAuthToken()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	for {
+		if loginSessionCheck(authToken) {
+			fmt.Println("Login session is valid")
+			break
+		}
+
 		fmt.Println("Enter your username: ")
 		fmt.Scan(&Username)
 		fmt.Println("Enter your password: ")
 		fmt.Scan(&Password)
 		fmt.Println("Making POST request...")
-		
+
 		Login(Username, Password)
 		if authCookie != "" {
 			break
 		}
-		
+
 		fmt.Println("Login failed. Would you like to retry? (y/n)")
 		fmt.Scan(&choice)
 		if choice != "y" {
@@ -36,30 +47,45 @@ func main() {
 			return
 		}
 	}
-	
-	fmt.Println("Enter the 2FA email OTP code: ")
-	fmt.Scan(&code)
-	VerifyEmailOTP(code)
-		
+
 	for {
-		/*
-		fmt.Println("Enter the Avatar ID you want to select: ")
-		fmt.Scan(&avatarID)
-			
-		ChangeAvatar(authCookie, avatarID)
-		
-	*/
-		fmt.Println("input userID | ")
-		fmt.Scan(&userID)
-		
-		sendFriendRq(authCookie, userID)
-	
-		fmt.Println("Select a different Avatar? Enter 'y' to continue, 'n' to exit program: ")
-		fmt.Scan(&choice)
-	
-		if choice != "y" {
-			fmt.Println("Exiting program.")
-			return
+		if loginSessionCheck(authToken) {
+			fmt.Println("Login session is valid")
+			break
 		}
+
+		fmt.Println("Enter the 2FA email OTP code: ")
+		fmt.Scan(&code)
+		VerifyEmailOTP(code)
 	}
+
+	fmt.Println("input AvatarID | ")
+	fmt.Scan(&avatarID)
+
+	getAvatar(avatarID)
+
+	/*
+		for {
+
+			fmt.Println("Enter the Avatar ID you want to select: ")
+			fmt.Scan(&avatarID)
+
+			ChangeAvatar(authCookie, avatarID)
+
+
+			fmt.Println("input userID | ")
+			fmt.Scan(&userID)
+
+			sendFriendRq(authCookie, userID)
+
+			fmt.Println("Select a different Avatar? Enter 'y' to continue, 'n' to exit program: ")
+			fmt.Scan(&choice)
+
+			if choice != "y" {
+				fmt.Println("Exiting program.")
+				return
+			}
+
+		}
+	*/
 }
